6e8dc4a8f44cc69cd537955e651496999d108cbe
kartikrathi089 <kartikrathi089@gmail.com>
2026-06-03 12:08:37 +0530

    intial cookies

--- src/main/java/msg.java
+++ src/main/java/msg.java
@@ -1,5 +1,6 @@
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,10 +12,29 @@ public class msg extends HttpServlet {
     @Override
     protected void service( HttpServletRequest req, HttpServletResponse res)
             throws IOException , ServletException {
-        int n1 =Integer.parseInt(req.getParameter("num1"));
-        int n2=Integer.parseInt(req.getParameter("num2"));
-        int add=n1+n2;
-        res.sendRedirect("add?k="+add);
+       String n1=req.getParameter("name");
+       PrintWriter out=res.getWriter();
+       boolean userVisit=false;
+       Cookie[] cookies= req.getCookies();
+       if(cookies!=null){
+           for(int i=0;i<cookies.length;i++){
+               Cookie c=cookies[i];
+               String name=c.getName();
+               String value=c.getValue();
+               if(name.equals("k") && value.equals("123")){
+                   out.print("welcome back "+n1);
+                   userVisit=true;
+                   break;
+               }
+           }
+       }
+       if(userVisit==false){
+           Cookie c1=new Cookie("k","123");
+           res.addCookie(c1);
+           out.print("welcome "+ n1);
+
+       }
+
     }
 
 }
