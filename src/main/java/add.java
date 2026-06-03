e074ac7c95551896986d1977135b505b10005bbb
kartikrathi089 <kartikrathi089@gmail.com>
2026-06-03 12:50:19 +0530

    HttpSession

--- src/main/java/add.java
+++ src/main/java/add.java
@@ -1,19 +1,20 @@
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/add")
 public class add extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
-        int num=Integer.parseInt(req.getParameter("k"));
-        //this technique nis known as url redirecting in we get parameter from url
-        int sq=num*num;
+        HttpSession session=req.getSession();
+        int n=(int)session.getAttribute("k");
+        int sq=n*n;
         PrintWriter out=res.getWriter();
-        out.print("Square ="+sq);
-
-
+        out.print("square of the number: "+sq);
 
     }
 }
--- src/main/java/msg.java
+++ src/main/java/msg.java
@@ -1,39 +1,21 @@
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/message")
 public class msg extends HttpServlet {
     @Override
     protected void service( HttpServletRequest req, HttpServletResponse res)
             throws IOException , ServletException {
-       String n1=req.getParameter("name");
-       PrintWriter out=res.getWriter();
-       boolean userVisit=false;
-       Cookie[] cookies= req.getCookies();
-       if(cookies!=null){
-           for(int i=0;i<cookies.length;i++){
-               Cookie c=cookies[i];
-               String name=c.getName();
-               String value=c.getValue();
-               if(name.equals("k") && value.equals("123")){
-                   out.print("welcome back "+n1);
-                   userVisit=true;
-                   break;
-               }
-           }
-       }
-       if(userVisit==false){
-           Cookie c1=new Cookie("k","123");
-           res.addCookie(c1);
-           out.print("welcome "+ n1);
+       int n1= Integer.parseInt(req.getParameter("name"));
+        HttpSession session=req.getSession();
+        session.setAttribute("k",n1);
+        res.sendRedirect("add");
 
-       }
 
     }
 
